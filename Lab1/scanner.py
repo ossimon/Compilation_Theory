c5a04c1a6a6693580eb89f79a3b9249c91d2dae5
Paulina100 <paulina.jedrychowska@gmail.com>
2022-11-13 17:37:30 +0100

    Reserved

--- Lab1/scanner.py
+++ Lab1/scanner.py
@@ -11,7 +11,7 @@ tokens = (
     'SEMICOLON', 'COMMA',
     'IF', 'ELSE', 'FOR', 'WHILE',
     'BREAK', 'CONTINUE', 'RETURN',
-    'EYE', 'ZEROS', 'ONCE',
+    'EYE', 'ZEROS', 'ONES',
     'PRINT',
     'ID',
     'FLOAT',
@@ -51,6 +51,32 @@ t_IF = r'if'
 t_ELSE = r'else'
 t_FOR = r'for'
 t_WHILE = r'while'
+t_BREAK = r'break'
+t_CONTINUE = r'continue'
+t_RETURN = r'return'
+t_EYE = r'eye'
+t_ZEROS = r'zeros'
+t_ONES = r'ones'
+t_PRINT = r'print'
+
+reserved = {
+    'if': 'IF',
+    'else': 'ELSE',
+    'for': 'FOR',
+    'while': 'WHILE',
+    'break': 'BREAK',
+    'continue': 'CONTINUE',
+    'return': 'RETURN',
+    'eye': 'EYE',
+    'zeros': 'ZEROS',
+    'ones': 'ONES',
+    'print': 'PRINT'
+}
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
 
 
 class Lexer:
