e17aeb662704530e27440797cfe71b6f44de1bc3
Oskar Simon <oskarsimon3@gmail.com>
2022-11-28 20:43:12 +0100

    added some productions

--- Lab2_Parser/Mparser.py
+++ Lab2_Parser/Mparser.py
@@ -16,6 +16,8 @@ precedence = (
     ('right', 'TRANSPOSE') # ????
 )
 
+start = 'program'
+
 
 def p_error(p):
     if p:
@@ -31,9 +33,9 @@ def p_program(p):
 
 def p_block(p):
     """block : LCURLBRACK block RCURLBRACK
+             | LCURLBRACK empty RCURLBRACK
              | block instruction
-             | instruction
-             | empty"""
+             | instruction"""
 
 
 def p_empty(p):
@@ -42,10 +44,70 @@ def p_empty(p):
 
 
 def instruction(p):
-    """instruction : assignment
-                   | call
+    """instruction : assignment SEMICOLON
+                   | call SEMICOLON
                    | loop
                    | branch"""
 
+def p_assignment(p):
+    """assignment : ID ASSIGN expression
+                  | ID ADDASSIGN expression
+                  | ID SUBASSIGN expression
+                  | ID MULASSIGN expression
+                  | ID DIVASSIGN expression"""
+
+
+def p_call(p):
+    """call : sys_call
+            | fun_call"""
+
+
+def p_sys_call(p):
+    """sys_call : BREAK
+                | CONTINUE
+                | RETURN expression"""
+
+
+def p_fun_call(p):
+    """fun_call : fun_name LPARENT expression RPARENT"""
+
+
+def p_fun_name(p):
+    """fun_name : EYE
+                | ZEROS
+                | ONES
+                | PRINT"""
+
+
+def p_loop(p):
+    """loop : for
+            | while"""
+
+
+def p_for(p):
+    """for : FOR for_expression block"""
+
+
+def p_for_expression(p):
+    """for_expression : ID ASSIGN term COLON term"""
+
+
+def p_term(p):
+    """term : ID
+            | number
+            | matrix"""
+
+
+def p_number(p):
+    """number : INT
+              | FLOAT"""
+
+
+def p_matrix(p):
+    """matrix : LSQBRACK"""
+
+
+def p_branch(p):
+    """"""
 
 parser = yacc.yacc()
