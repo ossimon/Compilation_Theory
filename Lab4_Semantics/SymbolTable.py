d473fc46065f7a6ed4b4401b83a1f1b71e70c342
Paulina100 <paulina.jedrychowska@gmail.com>
2023-01-09 21:39:20 +0100

    symbol table

--- Lab4_Semantics/SymbolTable.py
+++ Lab4_Semantics/SymbolTable.py
@@ -1,36 +1,44 @@
 #!/usr/bin/python
+from collections import defaultdict
+
+
+class Symbol(object):
+    pass
 
 
 class VariableSymbol(Symbol):
 
     def __init__(self, name, type):
-        pass
-    #
+        self.name = name
+        self.type = type
 
 
 class SymbolTable(object):
 
-    def __init__(self, parent, name): # parent scope and symbol table name
-        pass
-    #
+    def __init__(self, parent=None, name=''): # parent scope and symbol table name
+        self.symbols = defaultdict()
+        self.parent = parent
+        self.name = name
 
     def put(self, name, symbol): # put variable symbol or fundef under <name> entry
-        pass
-    #
+        self.symbols[name] = symbol
 
     def get(self, name): # get variable symbol or fundef from <name> entry
-        pass
-    #
+        if name in self.symbols.keys():
+            return self.symbols[name]
+
+        if self.parent is not None:
+            return self.parent.get(name)
+
+        return None
 
     def getParentScope(self):
-        pass
-    #
+        return self.parent
 
     def pushScope(self, name):
-        pass
-    #
+        return SymbolTable(parent=self, name=name)
 
     def popScope(self):
-        pass
-    #
+        self.symbols = None
+        return self.parent
 
